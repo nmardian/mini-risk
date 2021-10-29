e9fb7d0533ea04b8f285e56b044bc3779eb18e62
nmardian <nick.mardian@gmail.com>
2021-10-29 15:32:50 -0700

    Initial add and generate_game_board()

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use std::env;
+use std::collections::HashMap;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+#[derive(Debug)]
+struct Territory {
+    id: u32,
+    num_dice: u32,
+    owner_id: u32,
+}
+
+fn generate_gameboard(num_players: u32, num_territories_per_player: u32, num_dice_per_player: u32) -> HashMap<u32, Territory> {
+    
+    let max_territories = num_territories_per_player * num_players;
+
+    let mut territory_map: HashMap<u32, Territory> = HashMap::new();
+
+    for cur_id in 0..(max_territories) {
+        let cur_territory = Territory {
+            id: cur_id,
+            num_dice: 0,
+            owner_id: 0
+        };
+
+        territory_map.insert(cur_territory.id, cur_territory);
+    }
+
+    let mut territory_ids: Vec<u32> = (0..max_territories).collect();
+
+    let mut rng = thread_rng();
+    territory_ids.shuffle(&mut rng);
+    
+    let mut cur_player_id: u32 = 0;
+    for cur_territory in territory_ids {
+        cur_player_id = cur_player_id % num_players;
+        let mut found_terr: &mut Territory = match territory_map.get_mut(&cur_territory) {
+            Some(territory) => territory,
+            None => panic!("Error: No territory with ID {}", cur_territory),
+        };
+        found_terr.owner_id = cur_player_id;
+        cur_player_id += 1;
+    }
+
+    // this evenly distributes dice, need to make it random
+    for cur_player_id in 0..num_players {
+        let mut assigned_dice: u32 = 0;
+        while assigned_dice < num_dice_per_player {
+            for cur_territory_id in 0..max_territories {
+                let mut found_terr: &mut Territory = match territory_map.get_mut(&cur_territory_id) {
+                    Some(territory) => territory,
+                    None => panic!("Error: No territory with ID {}", cur_territory_id),
+                };
+                
+                if cur_player_id == found_terr.owner_id {
+                    found_terr.num_dice += 1;
+                    assigned_dice += 1;
+                }
+            }
+        }
+    }
+
+    println!("{:#?}", territory_map);
+
+    territory_map
+}
+
+fn main() {
+
+    let args : Vec<String> = env::args().collect();
+
+    if args.len() < 4 {
+        panic!("Usage: mini-risk <num players> <num territories per player> <num dice per player>");
+    }
+
+    let num_players: u32 = args[1].parse().expect("num players must be a positive integer");
+    let num_territories_per_player: u32 = args[2].parse().expect("num player territories per player must be a positive integer");
+    let num_dice_per_player: u32 = args[3].parse().expect("num dice per player must be a positive integer");
+
+    let all_territories: HashMap<u32, Territory> = generate_gameboard(num_players, num_territories_per_player, num_dice_per_player);
+    
+}
