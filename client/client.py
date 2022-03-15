6323d3c376a1725bac795115bc70975f0e96efde
nmardian <nick.mardian@gmail.com>
2022-03-14 17:30:06 -0700

    Show neighbors in client.

--- client/client.py
+++ client/client.py
@@ -5,18 +5,18 @@ import dataclasses
 import json
 
 def parseGameboard():
-    gameboardFile =  open('gameboard.json')
+    gameboard_file =  open('gameboard.json')
 
-    territoryMap = json.load(gameboardFile)
-    print(territoryMap)
+    territory_map = json.load(gameboard_file)
+    print(territory_map)
     
-    territoryList = []
-    for id in territoryMap['territory_map']:
-        terrData = territoryMap['territory_map'][id]
-        currTerritory = Territory(terrData['id'], terrData['num_dice'], terrData['owner_id'], terrData['neighbors'])
-        territoryList.append(currTerritory)
+    territory_list = []
+    for id in territory_map['territory_map']:
+        terr_data = territory_map['territory_map'][id]
+        curr_territory = Territory(terr_data['id'], terr_data['num_dice'], terr_data['owner_id'], terr_data['neighbors'])
+        territory_list.append(curr_territory)
 
-    return territoryList   
+    return territory_list   
 
 @dataclasses.dataclass
 class Territory:
@@ -34,13 +34,7 @@ screen = pygame.display.set_mode([window_width, window_height])
 
 font = pygame.font.SysFont(None, 16)
 
-territoryOne = Territory(0, 1, 1, [1, 2])
-territoryTwo = Territory(3, 1, 0, [1])
-territoryThree = Territory(1, 1, 1, [0, 2, 3])
-territoryFour = Territory(2, 1, 0, [1, 0])
-
-#territoryList = [territoryOne, territoryTwo, territoryThree, territoryFour]
-territoryList = parseGameboard()
+territory_list = parseGameboard()
 
 running = True
 while running:
@@ -51,22 +45,29 @@ while running:
     screen.fill((255, 255, 255))
 
     rotation = 0.0
-    delta_rotation = (2.0 * 3.14159) / len(territoryList)
+    delta_rotation = (2.0 * 3.14159) / len(territory_list)
     radius = 200.0
 
     x_origin = window_width / 2.0
     y_origin = window_height / 2.0
     pygame.draw.circle(screen, (0, 0, 0), (x_origin, y_origin), 10)
 
-    for cur_terr in territoryList:
+    coord_map = {}
+
+    for cur_terr in territory_list:
         x = (radius * math.sin(rotation)) + x_origin
         y = (radius * math.cos(rotation)) + y_origin
         pygame.draw.circle(screen, (0, 0, 255), (x, y), 50)
+        coord_map[cur_terr.id] = (x,y)
 
         terr_id_text_img = font.render(str(cur_terr.id), True, (0, 0, 0))
         screen.blit(terr_id_text_img, (x, y))
 
         rotation += delta_rotation
+
+    for cur_terr in territory_list:
+        for cur_neighbor in cur_terr.neighbors:
+            pygame.draw.line(screen, (255, 0, 0), coord_map[cur_terr.id], coord_map[cur_neighbor])
 
     pygame.display.flip()
 
